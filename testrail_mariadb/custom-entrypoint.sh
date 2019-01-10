ffc7cd05f8aeca96f3a3cd4dcd0048a188a06382
cbreit <christian@cbreit.net>
2019-01-10 19:07:24 +0100

    Fixed DB download process Fixed automatic background task spawning

--- testrail_mariadb/custom-entrypoint.sh
+++ testrail_mariadb/custom-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo $@
 
-if [ $# -gt 1 ]
+if [ $# -gt 0 ]
   then
     for i in $@; do :; done
     dbDump=$i
@@ -15,14 +15,19 @@ if [ $# -gt 1 ]
     
 
       #remove last argument
+      echo "###############################"
+      echo "DB downloaded"
       length=$(($#-1))
       array=${@:1:$length}
-      echo $array
-      exec /docker-entrypoint.sh mysqld "$array"
+
+      echo "Starting mariadb with the following arguments:"
+      echo "${array}"
+      echo "###############################"
+      exec /docker-entrypoint.sh mysqld"${array}"
     fi
   else
     echo "No DB dump argument provided"
     echo "mysqld $@"
-    exec /docker-entrypoint.sh mysqld "$@"
+    exec /docker-entrypoint.sh mysqld"$@"
 fi
 
---
+++ testrail_php_fpm/custom-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#set -e
+
+echo "##############"
+echo "Waiting for background task file"
+while [ ! -f /var/www/testrail/task.php ]
+do
+  sleep 2
+done
+
+echo "Starting background task"
+while /bin/true; do
+    php /var/www/testrail/task.php || true
+    sleep 60
+done &
+echo "##############"
+
+docker-php-entrypoint "$@"
+
